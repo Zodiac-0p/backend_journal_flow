191d84f7c5f8e7606bc669dd5feb94e3cd535bdf
Sanju s Kumar <sanjuskumarkply@gmail.com>
2026-08-18 10:56:07 +0530

    settings update

--- backend/settings.py
+++ backend/settings.py
@@ -142,6 +142,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
         'OPTIONS': {
             'timeout': int(os.getenv('SQLITE_TIMEOUT', '20')),
+            'CONN_MAX_AGE': 60,
         },
     }
 }
